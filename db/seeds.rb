2c5957a1737c1c0b1253f3e7399a2eb7090c163b
rexcklee <rexcklee852@gmail.com>
2024-10-22 11:29:40 -0500

    rewrite seeds.rb to pulls in product and category data from products.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-require 'faker'
+# require 'faker'
+
+# Product.delete_all
+
+# 676.times do
+#     Product.create(title:Faker::Commerce.unique.product_name, 
+#                    price:Faker::Commerce.price, 
+#                    stock_quantity: Faker::Number.number(digits: 4))
+# end
+
+require "csv"
 
 Product.delete_all
+Category.delete_all
 
-676.times do
-    Product.create(title:Faker::Commerce.unique.product_name, 
-                   price:Faker::Commerce.price, 
-                   stock_quantity: Faker::Number.number(digits: 4))
+filename = Rails.root.join("db/products.csv")
+puts "Loading data from the file: #{filename}"
+
+csv_data = File.read(filename)
+
+products = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+products.each do |product|
+    category = Category.find_or_create_by(name: product["category"])
+
+    if category&.valid?
+        new_product = category.products.create(
+            title: product["name"],
+            price: product["price"],
+            description: product["description"],
+            stock_quantity: product["stock quantity"]
+        )
+
+        if !new_product&.valid?
+            puts "Failed to create product: #{product["name"]}"
+            next
+        end
+    end
 end
