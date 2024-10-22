826556a1b601dc5936b106c407ea0267ee0f67e1
rexcklee <rexcklee852@gmail.com>
2024-10-22 11:08:14 -0500

    Add product show action/view and update index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'products' => 'products#index', as: 'products'
   get "products/index"
+
+  get "/products/:id", to: "products#show", as: "product", constraints: { id: /\d+/ } # company_path
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
